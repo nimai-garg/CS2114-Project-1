7efd23ef2f363e9e6aba603cc2bd4d8581d85f20
nolan-ayodeji <nolaneayodeji@gmail.com>
2026-09-22 15:36:02 -0400

    dispatcher finished

--- Dispatcher.java
+++ Dispatcher.java
@@ -1,8 +1,30 @@
 public class Dispatcher
 {
     //~ Fields ................................................................
+    /** current cash balance of the game's airline. */
+    private int cash;
 
+    /** current reputation score of the game's airline. */
+    private int reputation;
+
+    /** number of rounds successfully completed in the game. */
+    private int roundsCompleted;
+    // ----------------------------------------------------------
+    /**
+     * Create a new Dispatcher object.
+     * @param cash
+     * @param reputation
+     * @param roundsCompleted
+     */
     //~ Constructors ..........................................................
+    public Dispatcher(int cash, int reputation, int roundsCompleted) {
+        if (cash < 0 || reputation < 0 || roundsCompleted < 0) {
+            throw new IllegalArgumentException("Starting values cannot be negative.");
+        }
+        this.cash = cash;
+        this.reputation = reputation;
+        this.roundsCompleted = roundsCompleted;
+    }
 
     //~Public  Methods ........................................................
 
